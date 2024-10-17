5be50ba8b41472c56d2b4199dfc386fa17c5b887
Whirr2005 <mattbullen18@gmail.com>
2024-10-17 23:18:31 +0100

    beautiful map

--- src/com/app/MapPanel.java
+++ src/com/app/MapPanel.java
@@ -10,20 +10,25 @@ import javax.imageio.ImageIO;
 import java.util.ArrayList;
 
 class MapPanel extends JPanel {
-    private BufferedImage mapImage; // To hold the map image
-    private ArrayList<Point> positions; // To store points that will be plotted
+    private BufferedImage ukMapImage;    // uk map image
+    private ArrayList<Point> positions;  // array for points to be plotted
 
-    // World Map bounds
-    private final double MIN_LAT = -90.0; // Minimum latitude
-    private final double MAX_LAT = 90.0;  // Maximum latitude
-    private final double MIN_LON = -180.0; // Minimum longitude
-    private final double MAX_LON = 180.0;  // Maximum longitude
-    private final int map_width = 1600;  // Maximum longitude
-    private final int map_height = 800;  // Maximum longitude
-    private final int map_offset_x = 200;  // Maximum longitude
+    // full world dimensions for use calculating lat and long
+    private final int map_width = 15000;
+    private final int map_height = 10500;
+    //offsets for moving the uk area into frame
+    private final int map_offset_x = 6980; // higher number moves uk left
+    private final int map_offset_y = 1750; // higher number moves uk up
 
-    // Predefined positions (UK outline)
+    // UK bounds (approximate)
+    private final double UK_TOP_LAT = 59.586128178;
+    private final double UK_BOTTOM_LAT = 49.957122;
+    private final double UK_LEFT_LON = -10.4415;
+    private final double UK_RIGHT_LON = 1.76297;
+
+    // Predefined positions (UK outline and more)
     private final double[][] predefinedLatLon = {
+            //for testing plotting points
             {51.5074, -0.1278},  // London
             {52.4862, -1.8904},  // Birmingham
             {53.4808, -2.2426},  // Manchester
@@ -32,102 +37,99 @@ class MapPanel extends JPanel {
             {55.8642, -4.2518},  // Glasgow
             {54.5973, -5.9301},  // Belfast
             {51.4816, -3.1791},   // Cardiff
-            {-1.289798175528336, 32.93535678638415}, // eye of aftrica
-            {41.556458030741055, -97.53642771952767}//USA BABYYYY
+            {50.081587108944056, -5.640682437188373} // Cornwall
     };
 
     public MapPanel() {
-        // Load the map image
+        // load the uk map
         try {
-            mapImage = ImageIO.read(new File("C:\\Users\\mrfoo\\IdeaProjects\\co2_project\\src\\world-map.jpg")); // Update this path as necessary
+            ukMapImage = ImageIO.read(new File("C:\\Users\\mrfoo\\IdeaProjects\\co2_project\\src\\uk-map.png")); // Adjust path
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-        positions = new ArrayList<>(); // Initialize the list of positions
+        positions = new ArrayList<>(); // init list of positions
 
-        // Add mouse listener to handle clicks
+        // mouse listener for clicks
         addMouseListener(new MouseAdapter() {
             @Override
             public void mousePressed(MouseEvent e) {
-                // Get the screen coordinates of the click
                 int x = e.getX();
                 int y = e.getY();
-
-                // Convert the clicked screen coordinates to geographical coordinates
                 double[] latLon = convertXYToLatLon(x, y);
 
-                // Print the required outputs
                 System.out.println("Screen coordinates: (" + x + ", " + y + ")");
                 System.out.println("Geographical coordinates: (" + latLon[0] + ", " + latLon[1] + ")");
 
-                // Add the clicked point to the positions list as a geographical point
                 positions.add(new Point(x, y)); // Store the screen position
-
-                // Repaint the panel to show the new point
                 repaint();
             }
         });
     }
 
-    // Method to convert latitude/longitude to screen coordinates based on the world map
+    // convert lat/long to x y coordinates using world map variables
     private double[] convertLatLonToXY(double lat, double lon) {
-        int mapWidth = map_width;   // Width of the displayed map
-        int mapHeight = map_height; // Height of the displayed map
+        double x = (lon + 180.0) * (map_width / 360.0) - map_offset_x;
+        double y = (90.0 - lat) * (map_height / 180.0) - map_offset_y;
 
-        // Map latitude and longitude to pixel coordinates
-        double x = (lon + 180.0 ) * (mapWidth / 360.0) -200; // Convert longitude to pixel x
-        double y = (90.0 - lat) * (mapHeight / 180.0); // Convert latitude to pixel y
-
-        return new double[]{x, y}; // Return the point in screen coordinates
+        return new double[]{x, y};
     }
 
-    // Method to convert screen coordinates to latitude/longitude
+    //  screen coordinates to lat/long by doing the reverse of previous
     private double[] convertXYToLatLon(int x, int y) {
-        int mapWidth = map_width;   // Width of the displayed map
-        int mapHeight = map_height; // Height of the displayed map
+        double lon = ((x + map_offset_x) / (double) map_width) * 360.0 - 180.0;
+        double lat = 90.0 - ((y + map_offset_y) / (double) map_height) * 180.0;
 
-        // Map pixel coordinates back to latitude and longitude
-        double lon = ((x+200) / (double) mapWidth ) * 360.0 - 180.0; // Convert pixel x to longitude
-        double lat = 90.0 - (y / (double) mapHeight) * 180.0; // Convert pixel y to latitude
-
-        return new double[]{lat, lon}; // Return the geographical coordinates
+        return new double[]{lat, lon};
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        // Draw the map image
-        if (mapImage != null) {
-            g.drawImage(mapImage, -200, 0, map_width, map_height, this);
+
+        // set background
+        setBackground(Color.decode("#24293e"));
+
+        // place uk map in correct position based on the furthest points of uk
+        if (ukMapImage != null) {
+            // chnage geo coordinates to screen coords (x,y)
+            double[] ukTopLeft = convertLatLonToXY(UK_TOP_LAT, UK_LEFT_LON);
+            double[] ukBottomRight = convertLatLonToXY(UK_BOTTOM_LAT, UK_RIGHT_LON);
+
+            // width and height of the uk map on the screen
+            int ukScreenWidth = (int) (ukBottomRight[0] - ukTopLeft[0]);
+            int ukScreenHeight = (int) (ukBottomRight[1] - ukTopLeft[1]);
+
+            // draw uk-map image
+            g.drawImage(ukMapImage, (int) ukTopLeft[0], (int) ukTopLeft[1], ukScreenWidth, ukScreenHeight, this);
         }
 
-        // Plot predefined positions (UK outline)
-        g.setColor(Color.BLUE); // Color for predefined points
+        // plot predefined positions
+        g.setColor(Color.decode("#f4a3a6")); // Color for predefined points
         for (double[] latLon : predefinedLatLon) {
             double[] screenCoords = convertLatLonToXY(latLon[0], latLon[1]);
-            g.fillOval((int) screenCoords[0] - 5, (int) screenCoords[1] - 5, 10, 10); // Draw a circle for each point
+            g.fillOval((int) screenCoords[0] - 5, (int) screenCoords[1] - 5, 15, 15); // Draw a circle for each point
         }
 
-        // Plot newly clicked positions
-        g.setColor(Color.RED); // Color for clicked points
+        // plot clicked positions
+        g.setColor(Color.decode("#8ef2e4")); // Color for clicked points
         for (Point position : positions) {
             g.fillOval(position.x - 5, position.y - 5, 10, 10); // Draw a circle for each point
         }
     }
 
-    // Static method to create and show the frame with the map
+    // create method called from app.java
     static void create() {
-        JFrame newframe = new JFrame("World Map Plotter");
+        JFrame newframe = new JFrame("UK Map Plotter");
         newframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        newframe.setSize(400, 400); // Adjust frame size as needed
+        newframe.setSize(700, 800); // Adjust frame size as needed
         newframe.setLocationRelativeTo(null); // Center the frame on the screen
         newframe.setResizable(false);
 
-        // Create an instance of the MapPanel and add it to the frame
+        // create the mapPanel
         MapPanel mapPanel = new MapPanel();
-        newframe.add(mapPanel); // Add the MapPanel to the JFrame
+        newframe.add(mapPanel);
 
-        newframe.setVisible(true); // Make the frame visible
+        newframe.setVisible(true);
     }
 }
